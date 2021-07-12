9d749616d98dfa0ef6253c6dc56291b8bfeb2525
Elton <elton.novais@sempreit.com.br>
2021-07-12 18:16:03 -0300

    first

---
+++ compraAzul/src/test/java/testCompra/CucumberMaker.java
@@ -0,0 +1,11 @@
+package testCompra;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\Sempre IT\\IdeaProjects\\compraAzul\\src\\test\\java\\feature", glue = "testPassos")
+
+public class CucumberMaker {
+}
