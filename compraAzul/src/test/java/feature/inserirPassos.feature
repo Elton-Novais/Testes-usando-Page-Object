42c9c2ce21728e8e1793b769525e9b14128e4135
Elton <elton.novais@sempreit.com.br>
2021-07-16 17:21:01 -0300

    Esse é o código de compra no site da Azul.

--- compraAzul/src/test/java/feature/inserirPassos.feature
+++ compraAzul/src/test/java/feature/inserirPassos.feature
@@ -8,11 +8,11 @@
   Cenario: Comprando passagem no site da Azul
     Dado que estou no site https://www.voeazul.com.br
     Quando preencho os dados referentes a viagem
-    Quando seleciono o vôo
+    Quando seleciono o voo
     Quando preencho dados do passageiro
     Quando escolho o assento
     Quando clico em prosseguir na tela de bagagens
     Quando preencho os dados de contato
     E marco a checkbox dizendo que Li e Concordo com as regras
-    Entao valido se o botão "Confirmar reserva" está habilitado
+    Entao valido se o botao Confirmar reserva esta habilitado
 
--- compraAzul/src/test/java/passosDefinidos/testPassos.java
+++ compraAzul/src/test/java/passosDefinidos/testPassos.java
@@ -12,18 +12,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.lang.model.element.Element;
-import javax.security.auth.x500.X500Principal;
-import javax.swing.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 
@@ -35,49 +27,41 @@ public class testPassos {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sempre IT\\IdeaProjects\\compraAzul\\Driver\\chromedriver.exe");
         navegador = new ChromeDriver();
         navegador.manage().window().maximize();
-        navegador.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
-    @Dado("que estou no site https:\\/\\/www.voeazul.com.br")
+    @Dado("que estou no site https://www.voeazul.com.br")
     public void que_estou_no_site_https_www_voeazul_com_br() {
-
         navegador.get("https://www.voeazul.com.br");
     }
 
     @Quando("preencho os dados referentes a viagem")
     public void preencho_os_dados_referentes_a_viagem() throws InterruptedException {
-
         WebDriverWait wait = new WebDriverWait(navegador, 5);
         wait.until(ExpectedConditions.elementToBeClickable(By.id("TCSS__tabbox-shopping")));
-
         navegador.findElement(By.xpath("//*[@value='OneWay']")).click();
-
         navegador.findElement(By.xpath("//input[@id='field-5-origin1']")).sendKeys("GRU");
         navegador.findElement(By.xpath("//input[@id='field-5-origin1']")).sendKeys(Keys.ENTER);
-
         Thread.sleep(2000);
         navegador.findElement(By.xpath("//input[@id='field-6-destination1']")).sendKeys("POA");
         navegador.findElement(By.xpath("//input[@id='field-6-destination1']")).sendKeys(Keys.ENTER);
-
         Random aleatorio = new Random();
         int dia = aleatorio.nextInt(120);
         LocalDate dataDePartida = LocalDate.now().plusDays(dia);
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        navegador.findElement(By.xpath("//input[@id='field-7-departure1']")).sendKeys((dataDePartida.format(formato)).toString());
-
-        navegador.findElement(By.id("searchTicketsButton")).click();
+        navegador.findElement(By.xpath("//input[@id='field-7-departure1']")).sendKeys((dataDePartida.format(formato)));
+        Thread.sleep(1000);
+        navegador.findElement(By.xpath("//button[@id='searchTicketsButton']")).click();
     }
 
-    @Quando("seleciono o vôo")
-    public void seleciono_o_vôo() throws InterruptedException {
-        Thread.sleep(4000);
+    @Quando("seleciono o voo")
+    public void seleciono_o_voo() throws InterruptedException {
+        Thread.sleep(3000);
         navegador.findElement(By.xpath("(//*[@id='AvailabilityInputAvailabilityView_LinkButtonSubmit'])[1]")).click();
     }
 
     @Quando("preencho dados do passageiro")
     public void preencho_dados_do_passageiro() throws InterruptedException {
-
         navegador.findElement(By.xpath("//input[@id='PassengerControlPassengerView_TextBoxFirstName_0']")).sendKeys("Erson");
         navegador.findElement(By.xpath("//input[@id='PassengerControlPassengerView_TextBoxLastName_0']")).sendKeys("Teste");
         navegador.findElement(By.xpath("//*[@id='countryDiv']/div/button")).click();
@@ -92,18 +76,11 @@ public class testPassos {
 
     @Quando("escolho o assento")
     public void escolho_o_assento() throws InterruptedException {
-        //List<String> letra = Arrays.asList("A", "B", "C", "D", "E", "F");
-        //String select = letra.get(new Random().nextInt(letra.size()));
         Random aleatorio = new Random();
         int poltrona = aleatorio.nextInt(60);
         navegador.findElement(By.xpath("(//a[contains(@class,'seat -available') and @unit_value_pax_0='R$ 0,00'])[" + poltrona + "]")).click();
-        if (navegador.findElement(By.xpath("//input[@id='termosCondicoesEmergency']")).isEnabled()) {
-            ((RemoteWebDriver) navegador).executeScript("scrollBy(0,950)", "");
-            navegador.findElement(By.xpath("//input[@id='termosCondicoesEmergency']")).click();
-            navegador.findElement(By.xpath("//button[@id='btnCloseConfirmEmergency']")).click();
-        }
-        Thread.sleep(9000);
-        navegador.findElement(By.xpath("//*[@id='btnProsseguir']")).click();
+        Thread.sleep(6000);
+        navegador.findElement(By.id("btnProsseguir")).click();
     }
 
     @Quando("clico em prosseguir na tela de bagagens")
@@ -125,17 +102,13 @@ public class testPassos {
         ((RemoteWebDriver) navegador).executeScript("scrollBy(0,950)", "");
     }
 
-    @Entao("valido se o botão {string} está habilitado")
-    public void valido_se_o_botão_está_habilitado(String string) {
-
-        //WebElement element = navegador.findElement(By.xpath("//button[@class='btn btn-action btn-next']"));
-        //boolean valido = element.isEnabled();
-        //if (valido == true) {
-        //JOptionPane.showMessageDialog(null,"Botão Ativado com Sucesso!!!!");
-        String ativo = navegador.findElement(By.xpath("//button[@class='btn btn-action btn-next']")).getText();
-        Assert.assertEquals("Confirmar reserva", ativo);
-        System.out.println(ativo);
-        }
+    @Entao("valido se o botao Confirmar reserva esta habilitado")
+    public void valido_se_o_botao_Confirmar_reserva_esta_habilitado() {
+        WebElement ativo = navegador.findElement(By.xpath("//button[@class='btn btn-action btn-next']"));
+        String texto = ativo.getText();
+        Assert.assertEquals("Confirmar reserva", texto);
+        System.out.println("================  "+texto+"  ==================");
+         }
     }
 
 
